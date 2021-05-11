a959c4a4e53a35542f1ef1153ca09401cc485d2e
Junior Castro <devcastrojunin@gmail.com>
2021-05-11 18:53:02 -0300

    Update and remove methods

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -1,43 +1,45 @@
 module Api
-    module V1
-        class RolesController < ActionController::API
-            def index
-                render json: Role.all
-            end
-    
-            def show
-                render json: Role.find(params[:id])
-            end
+  module V1
+    class RolesController < ActionController::API
+      def index
+        render json: Role.all
+      end
 
-            def create
-                role = Role.new(role_params)
-                if role.save
-                    render json: role
-                else
-                    render error: {error: "Unable to create role"}, status: 400                    
-                end
-            end
+      def show
+        render json: Role.find(params[:id])
+      end
 
-            def update
-               role = Role.update(role_params)
-               render json: role
-            end
-            def update
-                role = Role.update(role_params)
-                if role
-                  render json: role
-                else
-                  render json: role.errors, status: :unprocessable_entity
-                end
-              end
-            
-            private
-    
-            # Strong parameters
-            def role_params
-                params.require(:role)
-                    .permit(:nome, :status)
-            end
+      def create
+        role = Role.new(role_params)
+        if role.save
+          render json: role
+        else
+          render error: { error: "Unable to create role" }, status: 400
         end
+      end
+
+      def update
+        role = Role.find(params[:id])
+        if role.update(role_params)
+          render json: { status: "SUCCESS", message: "Updated role", data: role }, status: :ok
+        else
+          render json: role.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        role = Role.find(params[:id])
+        role.destroy
+        render json: { status: "SUCCESS", message: "Deleted role", data: role }, status: :ok
+      end
+
+      private
+
+      # Strong parameters
+      def role_params
+        params.require(:role)
+          .permit(:nome, :status)
+      end
     end
+  end
 end
